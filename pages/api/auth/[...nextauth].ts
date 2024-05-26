9d65301b031709547c3eb21737aa668b19a00131
Dinmukhammed Omirzak <030619501100@stud.satbayev.university>
2024-05-26 18:18:21 +0500

    last

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,8 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+            authorization:
+                'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         }),
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID || '',
