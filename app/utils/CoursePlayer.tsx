030cb414a59b4180426ece7039b24df0ffa1ad87
Dinmukhammed Omirzak <030619501100@stud.satbayev.university>
2024-06-06 21:25:09 +0500

    fix

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -39,9 +39,62 @@ const CoursePlayer: FC<Props> = ({ videoUrl, title }) => {
         //     )}
         // </div>
 
-        <div>
-            <script src="https://fast.wistia.com/embed/medias/6gexd62acq.jsonp" async></script>
-            <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+        <>
+            {/* <div>
+                <script src="https://fast.wistia.com/embed/medias/6gexd62acq.jsonp" async></script>
+                <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                <div
+                    className="wistia_responsive_padding"
+                    style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+                    <div
+                        className="wistia_responsive_wrapper"
+                        style={{
+                            height: '100%',
+                            left: '0',
+                            position: 'absolute',
+                            top: '0',
+                            width: '100%',
+                        }}>
+                        <div
+                            className="wistia_embed wistia_async_6gexd62acq seo=true videoFoam=true"
+                            style={{ height: '100%', position: 'relative', width: '100%' }}>
+                            <div
+                                className="wistia_swatch"
+                                style={{
+                                    height: '100%',
+                                    left: '0',
+                                    opacity: '0',
+                                    overflow: 'hidden',
+                                    position: 'absolute',
+                                    top: '0',
+                                    transition: 'opacity 200ms',
+                                    width: '100%',
+                                }}>
+                                {videoUrl !== '' && (
+                                    // eslint-disable-next-line @next/next/no-img-element
+                                    <img
+                                        src={`${videoData.videoUrl}/swatch`}
+                                        style={{
+                                            filter: 'blur(5px)',
+                                            height: '100%',
+                                            objectFit: 'contain',
+                                            width: '100%',
+                                        }}
+                                        alt=""
+                                        aria-hidden="true"
+                                        onLoad={(e) => {
+                                            const target = e.target as HTMLImageElement;
+                                            const parent = target.parentNode as HTMLElement;
+                                            parent.style.opacity = '1';
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
             <div
                 className="wistia_responsive_padding"
                 style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
@@ -49,50 +102,26 @@ const CoursePlayer: FC<Props> = ({ videoUrl, title }) => {
                     className="wistia_responsive_wrapper"
                     style={{
                         height: '100%',
-                        left: '0',
+                        left: 0,
                         position: 'absolute',
-                        top: '0',
+                        top: 0,
                         width: '100%',
                     }}>
-                    <div
-                        className="wistia_embed wistia_async_6gexd62acq seo=true videoFoam=true"
-                        style={{ height: '100%', position: 'relative', width: '100%' }}>
-                        <div
-                            className="wistia_swatch"
-                            style={{
-                                height: '100%',
-                                left: '0',
-                                opacity: '0',
-                                overflow: 'hidden',
-                                position: 'absolute',
-                                top: '0',
-                                transition: 'opacity 200ms',
-                                width: '100%',
-                            }}>
-                            {videoUrl !== '' && (
-                                // eslint-disable-next-line @next/next/no-img-element
-                                <img
-                                    src={`${videoData.videoUrl}/swatch`}
-                                    style={{
-                                        filter: 'blur(5px)',
-                                        height: '100%',
-                                        objectFit: 'contain',
-                                        width: '100%',
-                                    }}
-                                    alt=""
-                                    aria-hidden="true"
-                                    onLoad={(e) => {
-                                        const target = e.target as HTMLImageElement;
-                                        const parent = target.parentNode as HTMLElement;
-                                        parent.style.opacity = '1';
-                                    }}
-                                />
-                            )}
-                        </div>
-                    </div>
+                    <iframe
+                        src={`https://fast.wistia.net/embed/iframe/${videoData.videoUrl}?seo=true&videoFoam=true`}
+                        allow="autoplay; fullscreen"
+                        allowTransparency={true}
+                        frameBorder="0"
+                        scrolling="no"
+                        className="wistia_embed"
+                        name="wistia_embed"
+                        allowFullScreen
+                        width="100%"
+                        height="100%"></iframe>
                 </div>
             </div>
-        </div>
+            <script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
+        </>
     );
 };
 
